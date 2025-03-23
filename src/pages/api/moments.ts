462d3dffdd502c4abcb88a576e8d595fe36c9a7d
Brett Pierce <bapierce88@gmail.com>
2025-03-22 21:24:26 -0400

    cleaning more thangs

--- src/pages/api/moments.ts
+++ src/pages/api/moments.ts
@@ -5,10 +5,10 @@ import path from 'path';
 import sharp from 'sharp';
 
 const s3Client = new S3Client({
-  region: 'us-east-1', // replace with your region
+  region: 'us-east-1',
   credentials: {
-    accessKeyId: import.meta.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: import.meta.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: import.meta.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: import.meta.env.S3_SECRET_ACCESS_KEY,
   },
 });
 
